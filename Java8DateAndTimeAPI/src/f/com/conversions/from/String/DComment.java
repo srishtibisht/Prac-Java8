aa7d9f410240ca11b5fdb911749d5b68fea8cfc1
srishtibisht <srishtibisht29@gmail.com>
2022-10-17 15:38:36 +0530

    Extra class - Just for learning

---
+++ Java8DateAndTimeAPI/src/f/com/conversions/from/String/DComment.java
@@ -0,0 +1,10 @@
+package f.com.conversions.from.String;
+
+public class DComment {
+
+	public static void main(String[] args) {
+
+		System.out.println("This class is act as a extra class where i will mention the details of the project.");
+	}
+
+}
